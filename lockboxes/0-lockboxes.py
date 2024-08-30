d5f1648f6496fb64abc4c205726b11d2666a9d20
elyessassi <mjoxen@gmail.com>
2024-08-30 02:46:55 +0100

    testing in the checker

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -18,7 +18,7 @@ def canUnlockAll(boxes):
         print(f"boxes : {num_of_boxes_to_unlock}")
         print(f"numboxes : {len(boxes)}")
         print(f"test {boxes[0][0:3]}")
-        print(keys[-1])
+        print(boxes[999][0])
 
     if len(keys) == num_of_boxes_to_unlock:
         return (True)
