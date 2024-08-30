cec0484561923641725c8289c85fe3e23300cc57
elyessassi <mjoxen@gmail.com>
2024-08-30 02:11:06 +0100

    deleting print functions used to test

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -11,9 +11,6 @@ def canUnlockAll(boxes):
         for j in boxes[i]:
             if j not in keys and j != 0:
                 keys.append(j)
-                print(keys)
-    print(f"keys: {len(keys)}")
-    print(f"num: {num_of_boxes_to_unlock}")
     if len(keys) == num_of_boxes_to_unlock:
         return (True)
     else:
