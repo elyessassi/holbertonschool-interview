589932764d0fdb6d200aa173ca537dc1916d2285
elyessassi <mjoxen@gmail.com>
2024-10-13 18:41:21 +0100

    pep8 style

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -27,7 +27,6 @@ def main():
     try:
         for i in range(len(lines)):
             if re.findall(regex, lines[i]) == []:
-                print(f"this is the data {data}  end of data")
                 continue
             else:
                 num_lines += 1
