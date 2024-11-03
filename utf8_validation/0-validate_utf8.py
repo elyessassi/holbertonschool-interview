c21d4355e04969e3b233d43e117a116ed11f2bdd
elyessassi <mjoxen@gmail.com>
2024-11-03 16:33:01 +0100

    An algorithm that determines if a given data set represents a valid UTF-8 encoding

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -6,23 +6,30 @@ data set represents a valid UTF-8 encoding """
 def validUTF8(data):
     """ The function that does the task"""
     i = 0
-    byteCount = 0
-    while i < (len(data) - 1):
-        if data[i] > 255 or data[i] < 0:
-            return False
-        else:
-            if data[i] >> 5 == int("110", 2):
-                byteCount = 2
-            if data[i] >> 4 == int("1110", 2):
-                byteCount = 3
-            if data[i] >> 3 == int("11110", 2):
-                byteCount = 4
-        if byteCount > 0:
-            for j in range(1, byteCount + 1):
-                j = j + i
-                if data[j] >> 6 != int("10", 2):
+    j = 1
+    k = 0
+    following_bytes = 0
+    while i <= (len(data) - 1):
+        print(f"working on {i}")
+        data[i] = data[i] & 0xFF
+        if data[i] >> 5 == int("110", 2):
+            following_bytes = 1
+        elif data[i] >> 4 == int("1110", 2):
+            following_bytes = 2
+        elif data[i] >> 3 == int("11110", 2):
+            following_bytes = 3
+        if following_bytes > 0:
+            while True:
+                k = i + j
+                if k > len(data) - 1:
+                    return False
+                elif data[k] >> 6 != int("10", 2):
                     return False
                 i = i + 1
+                if k == following_bytes:
+                    i = i + 1
+                    following_bytes = 0
+                    break
         else:
             i = i + 1
     return True
