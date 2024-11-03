cb2bb48132333ede63ba4080dcad4122dc7c901f
elyessassi <mjoxen@gmail.com>
2024-11-03 17:32:29 +0100

    improving the logic used when looking for following byte

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -6,7 +6,7 @@ data set represents a valid UTF-8 encoding """
 def validUTF8(data):
     """ The function that does the task"""
     i = 0
-    following_bytes = 1
+    following_bytes = 0
     while i <= (len(data) - 1):
         data[i] = data[i] & 0xFF
         if data[i] >= 250:
@@ -17,12 +17,12 @@ def validUTF8(data):
             following_bytes = 2
         elif data[i] >> 3 == int("11110", 2):
             following_bytes = 3
-        if following_bytes > 1:
+        if following_bytes > 0:
             for j in range(following_bytes):
                 if (j + i + 1) > len(data) - 1:
                     return False
                 elif data[j + i + 1] >> 6 != int("10", 2):
                     return False
-        i = i + following_bytes
-        following_bytes = 1
+        i = i + following_bytes + 1
+        following_bytes = 0
     return True
