5bb561d8fc4ae51bbea04596fe14ba06a9e3cd2b
elyessassi <mjoxen@gmail.com>
2024-12-14 23:04:37 +0100

    making the names printed in the correct order

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -5,21 +5,39 @@ const args = process.argv;
 
 const url = `https://swapi-api.hbtn.io/api/films/${args[2]}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    return (error);
+function getChara () {
+  return new Promise((resolve, reject) => {
+    try {
+      request(url, (error, response, body) => {
+        const chars = JSON.parse(body);
+        resolve(chars.characters);
+        if (error) {
+          return (error);
+        }
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+async function ListOfNames () {
+  const charL = await getChara();
+  for (let i = 0; i < charL.length; i++) {
+    await getName(charL[i]);
   }
-  if (response.statusMessage === 'NOT FOUND') {
-    return (0);
-  }
-  const film = (JSON.parse(body)).characters;
-  film.map((chara) => {
-    request(chara, (error, response, body) => {
+}
+
+function getName (charurl) {
+  return new Promise((resolve, reject) => {
+    request(charurl, (error, response, body) => {
+      console.log((JSON.parse(body)).name);
+      resolve();
       if (error) {
         return (error);
       }
-      console.log((JSON.parse(body)).name);
     });
-    return (0);
   });
-});
+}
+
+ListOfNames();
