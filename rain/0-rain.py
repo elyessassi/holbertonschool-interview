7f404178f3002e9d626d33c564b6e8fca2d6e1b7
elyessassi <mjoxen@gmail.com>
2024-12-20 11:32:14 +0100

    using a boolean to check if w1 has a value or not

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -17,26 +17,19 @@ def rain(walls):
     w2 = 0
     started = False
     for i in range(len(walls)):
-        print(f"entering :: i = {i}  w1 = {w1}  w2 =  {w2}")
         if (walls[i] > 0) and (started is False):
             w1 = i
             started = True
-            print(f"exiting case 1 ::: i = {i}  w1 = {w1}  w2 =  {w2}")
             continue
         if (walls[w1] > 0) and (walls[w1 + 1] >= walls[w1]) and (w1 < len(walls) - 2):
             w1 = w1 + 1
-            print(f"exiting case 2 ::: i = {i}  w1 = {w1}  w2 =  {w2}")
             continue
         if (walls[w1] > 0) and (walls[i] > walls[w1 + 1]):
             w2 = i
-
             for j in range(w1 + 1, w2):
                 if (walls[w1] >= walls[w2]):
                     RainWater = RainWater + (walls[w2] - walls[j])
                 else:
                     RainWater = RainWater + (walls[w1] - walls[j])
             w1 = w2
-            print(f"""exiting  case 3 ::: i = {i}
-                  w1 = {w1}  w2 =  {w2}    RW = {RainWater}""")
-
     return RainWater
