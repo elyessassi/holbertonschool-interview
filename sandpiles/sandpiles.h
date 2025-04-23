46908bae9907cc906c0d340e012c4187b81731c4
elyessassi <mjoxen@gmail.com>
2025-04-23 12:14:39 +0100

    header file

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,11 @@
+#ifndef Sandpile
+#define Sandpile
+
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void my_print_grid(int grid[3][3]);
+int check_toppling(int grid[3][3]);
+int *sum_grid(int grid1[3][3], int grid2[3][3]);
+
+
+#endif
