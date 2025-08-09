0c5535cc606f57535feabd13a872451da7861142
elyessassi <mjoxen@gmail.com>
2025-08-09 14:47:17 +0100

    testing checker result

--- regex/regex.c
+++ regex/regex.c
@@ -1,45 +1,53 @@
 #include "regex.h"
 
 /**
- * 
- * 
+ * regex_match - checks if a pattern matches a string
+ * @str: string
+ * @pattern: the pattern
+ * Return: 1 if matches 0 if not
  */
 
-int regex_match(char const *str, char const *pattern){
+int regex_match(char const *str, char const *pattern)
+{
 
-    if (pattern == NULL || str == NULL)
-        return(0);
+	if (pattern == NULL || str == NULL)
+		return (0);
 
-    if (*pattern == '*')
-        return(0);
+	if (*pattern == '*')
+		return (0);
 
-    while (*str != '\0'){
-        if (*pattern == '\0')
-            return(0);
+	while (*str != '\0')
+	{
+		if (*pattern == '\0')
+			return (0);
 
-        if (*str == *pattern || *pattern == '.')
-        {
-            if (*(pattern + 1) != '*'){
-                str = str + 1;
-                pattern = pattern + 1;
-            }
-            else{
-                while((*str == *pattern || *pattern == '.') && *str != '\0' && *str != *(pattern + 2)){
-                    str = str + 1;
-                }
-                pattern = pattern + 2;
-            }
-        }
-        else{
-            if (*(pattern + 1) == '*')
-                pattern = pattern + 2;
-            else
-                return(0);
-        }
-            
-    }
-    if (*pattern == '\0' || *(pattern + 1) == '*'){
-        return(1);
-    }
-    return(0);
-}   
+		if (*str == *pattern || *pattern == '.')
+		{
+			if (*(pattern + 1) != '*')
+			{
+				str = str + 1;
+				pattern = pattern + 1;
+			}
+			else
+			{
+				while (*str == *pattern || *pattern == '.')
+				{
+					if (*str == '\0' || *str == *(pattern + 2))
+						break;
+					str = str + 1;
+				}
+				pattern = pattern + 2;
+			}
+		}
+		else
+		{
+			if (*(pattern + 1) == '*')
+				pattern = pattern + 2;
+			else
+				return (0);
+		}
+	}
+	if (*pattern == '\0' || *(pattern + 1) == '*')
+		return (1);
+	return (0);
+}
