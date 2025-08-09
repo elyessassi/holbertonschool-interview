4980dfbd21323ffa06e41678b4aa4203b12be75e
elyessassi <mjoxen@gmail.com>
2025-08-09 10:43:14 +0100

    regex algorithm

---
+++ regex/main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "regex.h"
+
+#define TEST_MATCH(s, p)    do {\
+    {\
+        int res = regex_match(s, p);\
+        printf("%s -> %s = %d\n", s, p, res);\
+    }\
+} while(0)
+
+/**
+ * main - Entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+    TEST_MATCH("H*Holberton", "Holberton");
+
+    return (EXIT_SUCCESS);
+}
+
+
+
---
+++ regex/regex.c
@@ -0,0 +1,42 @@
+#include "regex.h"
+
+/**
+ * 
+ * 
+ */
+
+int regex_match(char const *str, char const *pattern){
+
+    if (pattern == NULL || str == NULL)
+        return(0);
+
+    if (*pattern == '*')
+        return(0);
+
+    while (*str != '\0'){
+        if (*pattern == '\0')
+            return(0);
+
+        if (*str == *pattern || *pattern == '.')
+        {
+            if (*(pattern + 1) != '*'){
+                str = str + 1;
+                pattern = pattern + 1;
+            }
+            else{
+                while((*str == *pattern || *pattern == '.') && *str != '\0' && *str != *(pattern + 2)){
+                    str = str + 1;
+                }
+                pattern = pattern + 2;
+            }
+        }
+        else{
+            if (*(pattern + 1) == '*')
+                pattern = pattern + 2;
+            else
+                return(0);
+        }
+            
+    }
+    return(1);
+}   
