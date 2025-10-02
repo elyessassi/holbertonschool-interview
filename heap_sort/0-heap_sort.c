7589eeacc6d04aea54485e6302fbf6ac7f82d91d
elyessassi <mjoxen@gmail.com>
2025-10-03 00:52:51 +0100

    'handling_the_case_where_the_smaller_replaced_element_needs_to_get_swaped'

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -2,8 +2,9 @@
 
 // max is for the child that has the maximum value
 
-void heap_sort(int *array, size_t size){
-    int temp, i, j, maxidx;
+void heap_sort(int *array, size_t size)
+{
+    int temp, i, j, maxidx, checkIfChanged;
 
     if (size < 2)
         return;
@@ -11,49 +12,39 @@ void heap_sort(int *array, size_t size){
     if (array == NULL)
         return;
 
-
-    for (i = size - 1; i >= 0 ; i--){
-        for (j = i; j >= 0; j--){
+    for (i = size - 1; i >= 0; i--)
+    {
+        for (j = i; j >= 0; j--)
+        {
             // if it has left child
-            if ((j * 2 + 1) <= i){
+            if ((j * 2 + 1) <= i)
+            {
                 // check if it has right child
-                if ((j * 2 + 2) <= i){
-                    if (*(array + (j * 2 + 2)) > *(array + (j * 2 + 1))){
+                if ((j * 2 + 2) <= i)
+                {
+                    if (*(array + (j * 2 + 2)) > *(array + (j * 2 + 1)))
+                    {
 
                         maxidx = (j * 2 + 2);
                     }
-                    else{
+                    else
+                    {
                         maxidx = (j * 2 + 1);
                     }
                 }
                 else
                     maxidx = (j * 2 + 1);
-                if (*(array + maxidx) > *(array + j)){
+                if (*(array + maxidx) > *(array + j))
+                {
                     temp = *(array + maxidx);
                     *(array + maxidx) = *(array + j);
                     *(array + j) = temp;
                     print_array(array, size);
                     // this is for checking if the new changed child must be heapfied
-                    while((maxidx * 2 + 1) <= j){
-                        if ((maxidx * 2 + 1) <= j){
-                            if ((j * 2 + 2) > (j * 2 + 1))
-                                maxidx = (j * 2 + 2);
-                            else
-                                maxidx = (j * 2 + 1);
-                        }
-                        else
-                            maxidx = (j * 2 + 1);
+                    checkIfChanged = FixChangedElement(maxidx, &array, i);
+                    if (checkIfChanged == 1)
                         print_array(array, size);
-
-                        if (*(array + maxidx) > *(array + j)){
-                            temp = *(array + maxidx);
-                            *(array + maxidx) = *(array + j);
-                            *(array + j) = temp;
-                            print_array(array, size);
-                        }
-                    }
                 }
-
             }
         }
         // substituting the last element with the first element
@@ -64,4 +55,31 @@ void heap_sort(int *array, size_t size){
     }
 }
 
+int FixChangedElement(int indexOfElem, int **array, int LastIdx)
+{
+    int MaxChildIdx, temp, isChanged = 0;
 
+    while ((indexOfElem * 2) + 1 <= LastIdx)
+    {
+        if ((indexOfElem * 2) + 2 <= LastIdx)
+        {
+            if (*(*(array) + (indexOfElem * 2) + 2) > *(*(array) + (indexOfElem * 2) + 1))
+                MaxChildIdx = ((indexOfElem * 2) + 2);
+            else
+                MaxChildIdx = ((indexOfElem * 2) + 1);
+        }
+        else
+        {
+            MaxChildIdx = ((indexOfElem * 2) + 1);
+        }
+        if (*(*(array) + indexOfElem) < *(*(array) + MaxChildIdx))
+        {
+            isChanged = 1;
+            temp = *(*(array) + MaxChildIdx);
+            *(*(array) + MaxChildIdx) = *(*(array) + indexOfElem);
+            *(*(array) + indexOfElem) = temp;
+        }
+        indexOfElem = MaxChildIdx;
+    }
+    return (isChanged);
+}
