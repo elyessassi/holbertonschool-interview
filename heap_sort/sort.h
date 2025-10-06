ef8bd9e9174cfeae3533393e6bdbf25052f82cfa
elyessassi <mjoxen@gmail.com>
2025-10-06 22:55:40 +0100

    adding checkIfIdentical prototype

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -16,5 +16,6 @@ typedef struct node node_t;
 void heap_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void FixChangedElement(int indexOfElem, int **array, int LastIdx, size_t size);
+int checkIfIdentical(int *array, size_t size);
 
 #endif
