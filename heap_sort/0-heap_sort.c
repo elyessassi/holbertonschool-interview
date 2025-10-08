472654ef5ba3ca2ba2e5b3d2832a5922ff2c5579
elyessassi <mjoxen@gmail.com>
2025-10-08 10:27:14 +0100

    betty style

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -10,35 +10,23 @@ void heap_sort(int *array, size_t size)
 {
 	int temp, i, j, maxidx, isIdentical;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-
-	if (array == NULL)
-		return;
-
 	isIdentical = checkIfIdentical(array, size);
 	if (isIdentical == 1)
 		return;
-
 	for (i = size - 1; i >= 0; i--)
 	{
 		for (j = i; j >= 0; j--)
 		{
-			/**  if it has left child*/
-			if ((j * 2 + 1) <= i)
+			if ((j * 2 + 1) <= i) /**  if it has left child*/
 			{
-				/** check if it has right child */
-				if ((j * 2 + 2) <= i)
+				if ((j * 2 + 2) <= i) /** check if it has right child */
 				{
 					if (*(array + (j * 2 + 2)) > *(array + (j * 2 + 1)))
-					{
-
 						maxidx = (j * 2 + 2);
-					}
 					else
-					{
 						maxidx = (j * 2 + 1);
-					}
 				}
 				else
 					maxidx = (j * 2 + 1);
@@ -48,13 +36,11 @@ void heap_sort(int *array, size_t size)
 					*(array + maxidx) = *(array + j);
 					*(array + j) = temp;
 					print_array(array, size);
-					/** this is for checking if the new changed child must be heapfied */
-					FixChangedElement(maxidx, &array, i, size);
+					FixChangedElement(maxidx, &array, i, size); /** heapify new elem down*/
 				}
-			}   
+			}
 		}
-		/** substituting the last element with the first element */
-		if (*(array + i) != *array)
+		if (*(array + i) != *array) /**substituting last elem with first elem */
 		{
 			temp = *(array + i);
 			*(array + i) = *array;
@@ -65,60 +51,67 @@ void heap_sort(int *array, size_t size)
 }
 
 /**
- * FixChangedElement - heapifies changed element to the button so it takes its right spot
- * @indexOfElem: the index of the element we are heapifying
+ * FixChangedElement - heapifies changed elem to the bottom
+ * @index: the index of the element we are heapifying
  * @array: the array we are sorting
  * @LastIdx: the index that we shouldnt pass
  * @size: the size of the array
  * Return: 1 if changed 0 if not
  */
 
-void FixChangedElement(int indexOfElem, int **array, int LastIdx, size_t size)
+void FixChangedElement(int index, int **array, int LastIdx, size_t size)
 {
 	int MaxChildIdx, temp;
 
-	while ((indexOfElem * 2) + 1 <= LastIdx)
+	while ((index * 2) + 1 <= LastIdx)
 	{
-		if ((indexOfElem * 2) + 2 <= LastIdx)
+		if ((index * 2) + 2 <= LastIdx)
 		{
-			if (*(*(array) + (indexOfElem * 2) + 2) > *(*(array) + (indexOfElem * 2) + 1))
-				MaxChildIdx = ((indexOfElem * 2) + 2);
+			if (*(*(array) + (index * 2) + 2) > *(*(array) + (index * 2) + 1))
+				MaxChildIdx = ((index * 2) + 2);
 			else
-				MaxChildIdx = ((indexOfElem * 2) + 1);
+				MaxChildIdx = ((index * 2) + 1);
 		}
 		else
-		{
-			MaxChildIdx = ((indexOfElem * 2) + 1);
-		}
-		if (*(*(array) + indexOfElem) < *(*(array) + MaxChildIdx))
+			MaxChildIdx = ((index * 2) + 1);
+		if (*(*(array) + index) < *(*(array) + MaxChildIdx))
 		{
 			temp = *(*(array) + MaxChildIdx);
-			*(*(array) + MaxChildIdx) = *(*(array) + indexOfElem);
-			*(*(array) + indexOfElem) = temp;
+			*(*(array) + MaxChildIdx) = *(*(array) + index);
+			*(*(array) + index) = temp;
 			print_array(*array, size);
 		}
-		indexOfElem = MaxChildIdx;
+		index = MaxChildIdx;
 	}
 }
 
 
 
-/** I created this function so my output matches the required output when all elements are identical */
+/** Created for checker */
 
-int checkIfIdentical(int *array, size_t size){
+/**
+* checkIfIdentical - checking of all elems are identical in an array
+* @array: the array to check
+* @size: size of the array
+* Return: 1 if identical 0 if not
+*/
+
+int checkIfIdentical(int *array, size_t size)
+{
 	int isIdentical = 1, valueToCheck = *array;
 	size_t i;
 
-	for (i = 1; i < size; i++){
-		if (*(array + i) != valueToCheck){
-			
+	for (i = 1; i < size; i++)
+	{
+		if (*(array + i) != valueToCheck)
 			isIdentical = 0;
-		}
 	}
-	if (isIdentical == 1){
-		for (i = 0; i < size - 1; i++){
+	if (isIdentical == 1)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
 			print_array(array, size);
 		}
 	}
-	return(isIdentical);
-} 
+	return (isIdentical);
+}
