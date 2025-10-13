0399d8b8dadff93d62fbd551d54bad7758cc9949
elyessassi <mjoxen@gmail.com>
2025-10-13 17:34:03 +0100

    test

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -12,7 +12,10 @@ int main(int argc, char *argv[])
 	char *result, *start, *end, *num1 = argv[1], *num2 = argv[2];
 	int res_len, size1 = mystrlen(num1), size2 = mystrlen(num2);
 
-	 /**  mult is 1 then 10 then 100 ... */
+	if (*num1 == 0 && *num2 == 0){
+		printf("0");
+		exit(0);
+	}
 	if (argc != 3)
 		exitFunc();
 	if (checkIfNum(*num2) == 0)
