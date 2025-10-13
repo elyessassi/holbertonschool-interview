5a6dfe7e21f6d06ad0b71d15fd48d2fc4b8118ac
elyessassi <mjoxen@gmail.com>
2025-10-13 21:12:46 +0100

    adding documentation

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" Rotate Matrix Algorithm """
+
 
 def rotate_2d_matrix(matrix):
-	line_length = len(matrix)
-	
-	matrix.reverse()
+    """ Main function """
+    line_length = len(matrix)
+    matrix.reverse()
 
-	for i in range(line_length):
-		for j in range(i, line_length):
-			temp = matrix[i][j]
-			matrix[i][j] = matrix[j][i]
-			matrix[j][i] = temp
+    for i in range(line_length):
+        for j in range(i, line_length):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
