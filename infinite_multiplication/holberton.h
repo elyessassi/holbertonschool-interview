27f4491c18e16776050d3054fcc93353c4f9ad97
elyessassi <mjoxen@gmail.com>
2025-10-13 18:13:58 +0100

    adding addTwoDigitNum function prototype

--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -12,6 +12,7 @@ int checkIfNum(char chr);
 void exitFunc(void);
 void zerosArray(char **array, int len);
 char *getMul(char *n1, char *n2, int s1, int s2, char *res, char *s, char *e);
+void addTwoDigitNum(char **array, int number);
 
 
 #endif
