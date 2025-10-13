665c74290eb4c0df226893a61d06296a55652daa
elyessassi <mjoxen@gmail.com>
2025-10-13 12:12:56 +0100

    handling the case where all the numbers are zeros

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -15,6 +15,10 @@ int main(int argc, char *argv[]){
     }
     num1 = argv[1];
     num2 = argv[2];
+    if (*num2 == '0' || *num1 == 0){
+        printf("0\n");
+    }
+    
 
     if (checkIfNum(*num1) == 0 || checkIfNum(*num2) == 0){
         printf("Error\n");
