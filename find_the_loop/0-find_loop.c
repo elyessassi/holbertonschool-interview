840dbd6f0c98ed437075d8bc08750154d711651e
elyessassi <mjoxen@gmail.com>
2025-10-15 23:57:32 +0100

    betty style

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -1,31 +1,37 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * find_listint_loop - finding where the loop starts
+ * @head: first node of the list
+ * Return: pointer to the node where the loop starts
+ */
 
-listint_t *find_listint_loop(listint_t *head){
-    listint_t *slow;
-    listint_t *fast;
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow;
+	listint_t *fast;
 
-    if (head == NULL || head->next == NULL)
-        return(NULL);
+	if (head == NULL || head->next == NULL)
+		return (NULL);
 
-    slow = head->next;
-    fast = head->next->next;
-    
-    while (slow != fast){
-        slow = slow->next;
-        fast = fast->next->next;
-        
-        if (fast == NULL)
-            return(NULL);
-    }
+	slow = head->next;
+	fast = head->next->next;
 
-    
-    slow = head;
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == NULL)
+			return (NULL);
+	}
 
-    while(slow != fast){
-        slow = slow->next;
-        fast = fast->next;
-    }
-    return(fast);
+	slow = head;
+
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (fast);
 }
