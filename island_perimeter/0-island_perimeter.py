c60d2b6ddc55fb5cd9aa6c637722597e7b813bc9
elyessassi <mjoxen@gmail.com>
2025-10-19 11:59:22 +0100

    island perimeter algorithm

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    perimeter = 0
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i == 0):
+                    perimeter = perimeter + 1
+                elif (grid[i - 1][j] == 0):
+                    perimeter = perimeter + 1
+                if (i == len(grid) - 1):
+                    perimeter = perimeter + 1
+                elif (grid[i + 1][j] == 0):
+                    perimeter = perimeter + 1
+                if (j == 0):
+                    perimeter = perimeter + 1
+                elif (grid[i][j - 1] == 0):
+                    perimeter = perimeter + 1
+                if (j == len(grid[i]) - 1):
+                    perimeter = perimeter + 1
+                elif (grid[i][j + 1] == 0):
+                    perimeter = perimeter + 1
+
+    return(perimeter)
